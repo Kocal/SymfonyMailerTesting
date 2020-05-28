f605396acb6a7852b1f9bc774a1abd1e27f11787
RomulusED69 <redieux@gmail.com>
2020-05-28 11:45:15 +0200

    Fix assertions: add test includes

--- fixtures/applications/Symfony/src/Controller/EmailController.php
+++ fixtures/applications/Symfony/src/Controller/EmailController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,8 +20,8 @@ class EmailController extends AbstractController
     public function sendBasicEmail(Request $request, MailerInterface $mailer): Response
     {
         $email = (new Email())
-            ->from('symfony-mailer-testing@example.com')
-            ->to('john@example.com')
+            ->from(Address::fromString($request->request->get('from', 'symfony-mailer-testing@example.com')))
+            ->to(Address::fromString($request->request->get('to', 'john@example.com')))
             ->subject($request->request->get('subject', 'Email sent from a Symfony application!'))
             ->text($request->request->get('text', 'Hello world!'))
             ->html($request->request->get('html', '<b>Hello world!</b>'))
