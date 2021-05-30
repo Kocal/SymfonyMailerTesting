7b8f9b48f056d6c35da131d559d42532eb898302
Hugo Alliaume <hugo@alliau.me>
2021-05-30 07:50:52 +0200

    chore(deps): update PHP-CS-Fixer to ^3.0

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  '*.php': ['composer php-cs-fixer -- --config .php_cs --no-ansi', 'composer phpstan -- --no-ansi --no-progress'],
+  '*.php': [
+    'composer php-cs-fixer -- --config .php-cs-fixer.php --no-ansi',
+    'composer phpstan -- --no-ansi --no-progress',
+  ],
   '*.{js,jsx,vue}': ['yarn eslint --fix'],
   '*.{json,yml,yaml,md}': ['yarn prettier --write'],
 };
