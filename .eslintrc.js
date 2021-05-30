a2023343d9b889e70b56d085ec24ff2d3730a41a
Hugo Alliaume <hugo@alliau.me>
2021-05-30 13:42:41 +0200

    chore(pkg): use @kocal/eslint-config-generator and update ESLint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
-module.exports = {
-  root: true,
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  settings: {
-    'import/resolver': ['node'],
-  },
-};
+const { generateConfig } = require('@kocal/eslint-config-generator');
+
+const config = generateConfig();
+
+// We don't need @babel/eslint-parser as we don't use Babel
+delete config.parser;
+delete config.parserOptions;
+
+module.exports = config;
