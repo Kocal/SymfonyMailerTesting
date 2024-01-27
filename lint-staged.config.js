267168c6147ccd1f312d64e2d8cdc9cedd694df7
Hugo Alliaume <hugo@alliau.me>
2024-01-27 08:56:02 +0100

    chore(pkg): remove husky & lint-staged

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.php': [
-    'composer php-cs-fixer -- --config .php-cs-fixer.php --no-ansi',
-    'composer phpstan -- --no-ansi --no-progress',
-  ],
-  '*.{js,jsx,vue}': ['yarn eslint --fix'],
-  '*.{json,yml,yaml,md}': ['yarn prettier --write'],
-};
